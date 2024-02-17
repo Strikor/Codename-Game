c2072023fabaedff902e4a29dbb86f0f0834a69c
mohammadalashkar23 <mohammadalashkar@aucegypt.edu>
2024-02-17 01:53:11 -0500

    smooth moves
    you can move in 8 different directions instead of 4

--- Playground/sketch.js
+++ Playground/sketch.js
@@ -1,101 +1,81 @@
-//keeps track of screen space
 var view = {
     x: 0,
     y: 0,
     zoom: 1  
 };
 
-var Krill = {
+var krill = {
     x: 0,
     y: 0,
-    speed: 2,
-    direction: 0,
-    sprite: Image
+    speed: 200, // pixels per second
+    sprite: null
 };
 
+var lastFrameTime = 0;
+
 function preload() {
-    Krill.sprite = loadImage('assets/sideWalkL.gif');
+    krill.sprite = loadImage('assets/sideWalkL.gif');
 }
 
 function setup() {
-    createCanvas(400,400);
-    windowResized();
-
-}
-
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-    screen.width = windowWidth;
-    screen.height = windowHeight;
+    createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
+    var currentTime = millis();
+    var deltaTime = (currentTime - lastFrameTime) / 1000;
+    lastFrameTime = currentTime;
+
     background(220);
+
+    handleInput(deltaTime);
 
     translate(width / 2, height / 2);
     scale(view.zoom);
     translate(-view.x, -view.y);
 
-    //Impliment a level based draw system
-
-    // Draw objects
-    fill(32, 158, 74)
-    rect(-100, -200, 500, 500)
+    fill(32, 158, 74);
+    rect(-100, -200, 500, 500);
 
     fill(102);
     rect(0, 0, 63, 63);
     rect(100, 150, 63, 63);
 
-    //Draw Character
-    keyTyped();
-    image(Krill.sprite, Krill.x, Krill.y, -128, 64);
+    image(krill.sprite, krill.x, krill.y, -128, 64);
 
-    // Draw HUD elements
     resetMatrix();
     fill(0, 0, 0, 100);
     rect(0, 0, 140, 50);
-    
     fill(0);
     text(view.zoom + "x\n" + int(mouseX - width / 2) + ", " + int(mouseY - height / 2), 10, 20);
+}
+
+function handleInput(deltaTime) {
+    var speedPerFrame = krill.speed * deltaTime;
+
+    if (keyIsDown(87)) { // w
+        krill.y -= speedPerFrame;
+    }
+    if (keyIsDown(83)) { // s
+        krill.y += speedPerFrame;
+    }
+    if (keyIsDown(65)) { // a
+        krill.x -= speedPerFrame;
+    }
+    if (keyIsDown(68)) { // d
+        krill.x += speedPerFrame;
+    }
 }
 
 function mouseWheel(e) {
     view.zoom -= e.delta / 1000;
     view.zoom = constrain(view.zoom, 0.5, 5);
-  
 }
-
-function keyTyped() {
-    if (key.toLowerCase() == 'w') {
-        Krill.y -= Krill.speed;
-    } else if (key.toLowerCase() == 's') {
-        Krill.y += Krill.speed;
-    } else if (key.toLowerCase() == 'a') {
-        Krill.x -= Krill.speed;
-    } else if (key.toLowerCase() == 'd') {
-        Krill.x += Krill.speed;
-    }
-}
-
-function keyReleased() {
-    key = '';
-}
-
-// Fixed a problem with accelerated movement when mouse moved quickly by checking against the initial mouse position
-var initialMouseX, initialMouseY;
 
 function mousePressed() {
-    initialMouseX = mouseX;
-    initialMouseY = mouseY;
+    // Implement camera drag functionality
 }
-  
+
 function mouseDragged() {
-    var totalMouseX = mouseX - initialMouseX;
-    var totalMouseY = mouseY - initialMouseY;
-  
-    view.x -= totalMouseX / view.zoom;
-    view.y -= totalMouseY / view.zoom;
-  
-    initialMouseX = mouseX;
-    initialMouseY = mouseY;
-  }
+    // Implement camera drag functionality
+}
