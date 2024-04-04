6c465de4103ff2e1a00e9ed2f6f50878289c05ab
Ollena5 <anello1mail@gmail.com>
2024-04-04 14:54:03 -0400

    Just needed to save, will continue working later

---
+++ StoryScreen/sketch.js
@@ -0,0 +1,53 @@
+let mySound;
+let backgroundImage;
+let buttonImage;
+let buttonX;
+let buttonY;
+let buttonWidth = 200;
+let buttonHeight = 50;
+
+function preload() {
+    soundFormats("mp3");
+    mySound = loadSound('Krill (1).mp3');
+    // Load the background image
+    backgroundImage = loadImage('StoryScreen.png');
+    // Load the button image
+    buttonImage = loadImage('button.png'); // Replace 'button.png' with the path to your PNG button image
+}
+
+function setup() {
+    // Create the canvas
+    createCanvas(windowWidth, windowHeight);
+    backgroundMusic();
+
+    // Set button position
+    buttonX = width / 2 - buttonWidth / 2;
+    buttonY = height / 2 - buttonHeight / 2;
+}
+
+function backgroundMusic() {
+    mySound.play();
+    mySound.loop();
+    mySound.setVolume(0.5);
+    userStartAudio();
+}
+
+function draw() {
+    // Draw the background image
+    image(backgroundImage, 0, 0, width, height);
+    
+    // Draw the button image
+    image(buttonImage, buttonX, buttonY, buttonWidth, buttonHeight);
+}
+
+function switchToNewSketch() {
+    // Replace the current sketch with the new sketch
+    window.location.href = "KrillEditor\\sketch.js";
+}
+
+function mouseClicked() {
+    // Check if the mouse is over the button when clicked
+    if (mouseX > buttonX && mouseX < buttonX + buttonWidth && mouseY > buttonY && mouseY < buttonY + buttonHeight) {
+        switchToNewSketch();
+    }
+}
